0fcc42888b5596880342d1446f61568347dff0f4
igueyetraining <ismaelgueyetraining@gmail.com>
2025-04-27 16:19:45 -0400

    Add buildInfo

--- src/main/java/com/igueyetraining/helloworldrestapis/web/rest/HelloApi.java
+++ src/main/java/com/igueyetraining/helloworldrestapis/web/rest/HelloApi.java
@@ -1,23 +1,22 @@
 package com.igueyetraining.helloworldrestapis.web.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/hello")
-public class HelloApi{
-
-
+public class HelloApi {
 	
-    @GetMapping
+	@Autowired
+	BuildProperties buildProperties;
+
+    @GetMapping("/hello")
     public String hello(){
 		log.info("hello requested.... ");
-        return "Hello World";
+        return "Hello World [" + buildProperties.getName() + ":" + buildProperties.getVersion() + "]";
     }
-	
-	
 }
